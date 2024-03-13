cc9ebf6a732ffa7676602b9123e7ccbae7cef1c1
Pedro Pacheco <ppachecol95@gmail.com>
2024-03-13 19:53:11 -0300

    adicionar contagem ao console

---
+++ app.js
@@ -0,0 +1,24 @@
+let titulo = document.querySelector ('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function contarClique () {
+    let contador = 1;
+    let palavraVez = contador > 1 ? "vezes" : "vez";
+    console.log(`O botão foi clicado ${contador} ${palavraVez}`);
+    contador++;
+}
+
+function emitirAlerta () {
+    alert ('Brisa e Aurora gangsters');
+}
+
+function perguntarCidade () {
+    let cidade = prompt (`Digite o nome de uma cidade do Brasil:`);
+    alert (`Estive em ${cidade} e lembrei de você!`);
+}
+
+function somar() {
+    let numero1 = Number(prompt ('Digite um número:'));
+    let numero2 = Number(prompt ('Digite mais um número:'));
+    alert (`A soma de ${numero1} e ${numero2} é igual a ${numero1+numero2}`);
+}
